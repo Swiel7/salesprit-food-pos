5063c2e2af59b300cd11935d253ee8d0bac7ad9a
Swiel7 <s.wielgus11@gmail.com>
2024-07-01 23:56:25 +0200

    Integrate payments with Stripe

--- src/features/cart/CheckoutButton.tsx
+++ src/features/cart/CheckoutButton.tsx
@@ -1,10 +1,40 @@
+import { toast } from "react-toastify";
 import { Button } from "../../components";
+import { useState } from "react";
+import { useAppSelector } from "../../lib/store";
+import { UserService } from "../../lib/firestore-service";
+import { auth } from "../../lib/firebase.config";
+
+const url = import.meta.env.VITE_SERVER_URL;
 
 const CheckoutButton = () => {
-  const handleClick = () => console.log("checkout");
+  const { items } = useAppSelector((store) => store.cart);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleClick = async () => {
+    try {
+      setIsLoading(true);
+      const user = await UserService.getOne(auth.currentUser?.uid as string);
+
+      const response = await fetch(`${url}/create-checkout-session`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ cartItems: items, user }),
+      });
+
+      if (!response.ok) throw new Error("Something went wrong...");
+
+      const data = await response.json();
+      if (data?.url) window.location.href = data.url;
+    } catch (error) {
+      toast.error((error as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
-    <Button className="w-full" onClick={handleClick} loading={false}>
+    <Button className="w-full" onClick={handleClick} loading={isLoading}>
       Checkout
     </Button>
   );
